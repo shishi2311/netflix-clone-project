4e536aeecddf6f26bf07b34ef5b8018d4c378930
shishi2311 <ishita.bhatia1123@gmail.com>
2025-05-01 21:52:02 +0530

    fix: add typs

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,7 +1,14 @@
+/* eslint-disable */
 import { PrismaClient } from '@prisma/client';
 
-// @ts-ignore
-const client = global?.prismadb || new PrismaClient({
+// Extend the global type to include the prismadb property
+declare global {
+    // Use var instead of let to avoid block-scoping issues
+    var prismadb: PrismaClient | undefined;
+}
+
+// Adjust the global declaration to avoid redeclaration issues
+export const client = globalThis.prismadb ?? new PrismaClient({
     log: [
         {
             emit: 'event',
@@ -10,7 +17,6 @@ const client = global?.prismadb || new PrismaClient({
     ],
 });
 
-// @ts-ignore
-if (process.env.NODE_ENV === 'production') global.prismadb = client;
+globalThis.prismadb = client;
 
 export default client;
