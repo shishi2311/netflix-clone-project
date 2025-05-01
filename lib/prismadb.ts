080947b37b49b46362e92e55beebbc3ac132a741
shishi2311 <ishita.bhatia1123@gmail.com>
2025-05-01 21:38:54 +0530

    chore: add ts-ingore to ignore prismadb type issue

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+// @ts-ignore
 const client = global.prismadb || new PrismaClient({
     log: [
         {
@@ -8,6 +9,8 @@ const client = global.prismadb || new PrismaClient({
         },
     ],
 });
+
+// @ts-ignore
 if (process.env.NODE_ENV === 'production') global.prismadb = client;
 
 export default client;
